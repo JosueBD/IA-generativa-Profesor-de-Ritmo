9938ef8cd9ab52a79c4262555c77c083bf9dc826
JosueBD <62859587+JosueBD@users.noreply.github.com>
2025-05-19 22:21:20 -0400

    Ensure vite.config.js has correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/IA-generativa-Profesor-de-Ritmo/', // Ajusta al nombre del repositorio
+  base: '/IA-generativa-Profesor-de-Ritmo/',
   build: {
     outDir: 'dist',
   },
